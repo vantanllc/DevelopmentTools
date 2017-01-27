086c5d7d4015c363c6e12a061f0ee26b8c8fcad3
Thinh Luong <guitarslacker@gmail.com>
2017-01-26 22:56:08 -0600

    update to using test class

--- tests/test_get_png_from_svg.py
+++ tests/test_get_png_from_svg.py
@@ -10,30 +10,34 @@ OUTPUT_PNG_FLAG = "-e"
 
 SUBPROCESS_IMPORT = "inkscape.svg_to_png.get_png_from_svg.subprocess"
 
+class TestGetPngFromSvg(object):
+  subprocess = None
 
-@patch(SUBPROCESS_IMPORT)
-def test_get_png_from_svg__given_svg_file_and_output_path__returns_success_code(subprocess):
-  subprocess.call.return_value = 0
-  status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
-  assert status_code == 0
+  def setup_method(self):
+    self.subprocess_patch = patch(SUBPROCESS_IMPORT)
+    self.subprocess = self.subprocess_patch.start()
 
-
-@patch(SUBPROCESS_IMPORT)
-def test_get_png_from_svg__given_only_svg_file__returns_fail_code(subprocess):
-  subprocess.call.return_value = 0
-  status_code = get_png_from_svg(SVG_FILE_PATH, "")
-  assert status_code == -1
+  def teardown_method(self):
+    self.subprocess_patch.stop()
 
 
-@patch(SUBPROCESS_IMPORT)
-def test_get_png_from_svg__given_non_svg_file__returns_fail_code(subprocess):
-  subprocess.call.return_value = 0
-  non_svg_file = "music.mp3"
-  status_code = get_png_from_svg(non_svg_file, "")
-  assert status_code == -1
+  def test_get_png_from_svg__given_svg_file_and_output_path__returns_success_code(self):
+    self.subprocess.call.return_value = 0
+    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    assert status_code == 0
 
 
-@patch(SUBPROCESS_IMPORT)
-def test_get_png_from_svg__given_svg_file__calls_inkscape_with_svg_file(subprocess):
-  status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
-  subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, SVG_FILE_PATH])
+  def test_get_png_from_svg__given_only_svg_file__returns_fail_code(self):
+    status_code = get_png_from_svg(SVG_FILE_PATH, "")
+    assert status_code == -1
+
+
+  def test_get_png_from_svg__given_non_svg_file__returns_fail_code(self):
+    non_svg_file = "music.mp3"
+    status_code = get_png_from_svg(non_svg_file, "")
+    assert status_code == -1
+
+
+  def test_get_png_from_svg__given_svg_file__calls_inkscape_with_svg_file(self):
+    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, SVG_FILE_PATH])
