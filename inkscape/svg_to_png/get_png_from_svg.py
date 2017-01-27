033cadc8fcd269f54811efc7daac9e6bc1329bec
Thinh Luong <guitarslacker@gmail.com>
2017-01-26 22:47:08 -0600

    test subprocess call inkscape command

--- inkscape/svg_to_png/get_png_from_svg.py
+++ inkscape/svg_to_png/get_png_from_svg.py
@@ -1,6 +1,12 @@
-def get_png_from_svg(svg_file):
-  if svg_file.endswith("svg"):
-    return 0
+import subprocess
+
+INKSCAPE_COMMAND = "inkscape"
+NO_GUI_FLAG = "-z"
+OUTPUT_PNG_FLAG = "-e"
+  
+def get_png_from_svg(svg_file_path, output_file_path):
+  if svg_file_path.endswith("svg") and output_file_path.endswith("png"):
+    return subprocess.call([INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path])
   else:
     return -1
 
--- tests/test_get_png_from_svg.py
+++ tests/test_get_png_from_svg.py
@@ -1,11 +1,39 @@
 from inkscape.svg_to_png.get_png_from_svg import get_png_from_svg
+from mock import patch, mock
+import pytest
 
-def test_get_png_from_svg__given_svg_file__returns_success_code():
-  svg_file = "a_valid_svg_file.svg"
-  status_code = get_png_from_svg(svg_file)
+SVG_FILE_PATH = "a_valid_svg_file.svg"
+OUTPUT_FILE_PATH = "expected_png_file.png"
+INKSCAPE_COMMAND = "inkscape"
+NO_GUI_FLAG = "-z"
+OUTPUT_PNG_FLAG = "-e"
+
+SUBPROCESS_IMPORT = "inkscape.svg_to_png.get_png_from_svg.subprocess"
+
+
+@patch(SUBPROCESS_IMPORT)
+def test_get_png_from_svg__given_svg_file_and_output_path__returns_success_code(subprocess):
+  subprocess.call.return_value = 0
+  status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
   assert status_code == 0
 
-def test_get_png_from_svg__given_non_svg_file__returns_fail_code():
-  non_svg_file = "music.mp3"
-  status_code = get_png_from_svg(non_svg_file)
+
+@patch(SUBPROCESS_IMPORT)
+def test_get_png_from_svg__given_only_svg_file__returns_fail_code(subprocess):
+  subprocess.call.return_value = 0
+  status_code = get_png_from_svg(SVG_FILE_PATH, "")
   assert status_code == -1
+
+
+@patch(SUBPROCESS_IMPORT)
+def test_get_png_from_svg__given_non_svg_file__returns_fail_code(subprocess):
+  subprocess.call.return_value = 0
+  non_svg_file = "music.mp3"
+  status_code = get_png_from_svg(non_svg_file, "")
+  assert status_code == -1
+
+
+@patch(SUBPROCESS_IMPORT)
+def test_get_png_from_svg__given_svg_file__calls_inkscape_with_svg_file(subprocess):
+  status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+  subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, SVG_FILE_PATH])
