d57c5ef62aeb82f674fdc838e576d047164132fd
Thinh Luong <guitarslacker@gmail.com>
2017-01-30 15:16:50 -0600

    Refactor to take in width and height

--- inkscape/svg_to_png/get_png_from_svg.py
+++ inkscape/svg_to_png/get_png_from_svg.py
@@ -6,9 +6,10 @@ OUTPUT_PNG_FLAG = "-e"
 SVG_SUFFIX = "svg"
 PNG_SUFFIX = "png"
   
-def get_png_from_svg(svg_file_path, output_file_path):
+def get_png_from_svg(svg_file_path, output_file_path, width=None, height=None):
   if svg_file_path.endswith(SVG_SUFFIX) and output_file_path.endswith(PNG_SUFFIX):
-    return subprocess.call([INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path])
+    inkscape_call = [INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path]
+    return subprocess.call(inkscape_call)
   else:
     return -1
 
