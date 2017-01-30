fb88d11600047a44e839f29caa78b645d1e22149
Thinh Luong <guitarslacker@gmail.com>
2017-01-30 15:25:56 -0600

    Add capability to export width and height

--- inkscape/svg_to_png/get_png_from_svg.py
+++ inkscape/svg_to_png/get_png_from_svg.py
@@ -3,12 +3,19 @@ import subprocess
 INKSCAPE_COMMAND = "inkscape"
 NO_GUI_FLAG = "-z"
 OUTPUT_PNG_FLAG = "-e"
+WIDTH_FLAG = "-w"
+HEIGHT_FLAG = "-h"
 SVG_SUFFIX = "svg"
 PNG_SUFFIX = "png"
   
 def get_png_from_svg(svg_file_path, output_file_path, width=None, height=None):
   if svg_file_path.endswith(SVG_SUFFIX) and output_file_path.endswith(PNG_SUFFIX):
     inkscape_call = [INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path]
+    if width:
+      inkscape_call += [WIDTH_FLAG, width] 
+    if height:
+      inkscape_call += [HEIGHT_FLAG, height]
+      
     return subprocess.call(inkscape_call)
   else:
     return -1
--- tests/test_get_png_from_svg.py
+++ tests/test_get_png_from_svg.py
@@ -7,6 +7,10 @@ OUTPUT_FILE_PATH = "expected_png_file.png"
 INKSCAPE_COMMAND = "inkscape"
 NO_GUI_FLAG = "-z"
 OUTPUT_PNG_FLAG = "-e"
+WIDTH_FLAG = "-w"
+HEIGHT_FLAG = "-h"
+EXPECTED_WIDTH = 100
+EXPECTED_HEIGHT = 200
 
 SUBPROCESS_IMPORT = "inkscape.svg_to_png.get_png_from_svg.subprocess"
 
@@ -39,27 +43,35 @@ class TestGetPngFromSvg(object):
 
 
   def test_get_png_from_svg__given_svg_file_and_output_file_path__calls_inkscape(self):
-    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
     self.subprocess.call.assert_called_with([INKSCAPE_COMMAND, mock.ANY, mock.ANY, mock.ANY, mock.ANY])
   
 
   def test_get_png_from_svg__given_svg_file_and_output_file_path__calls_with_svg_file(self):
-    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
     self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, SVG_FILE_PATH])
 
 
   def test_get_png_from_svg__given_svg_file_and_output_file_path__calls_with_output_file(self):
-    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
     self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, OUTPUT_FILE_PATH, mock.ANY])
 
 
   def test_get_png_from_svg__given_svg_file_and_output_file_path__calls_with_no_gui_flag(self):
-    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
     self.subprocess.call.assert_called_with([mock.ANY, NO_GUI_FLAG, mock.ANY, mock.ANY, mock.ANY])
 
 
   def test_get_png_from_svg__given_svg_file_and_output_file_path__calls_with_output_png_flag(self):
-    status_code = get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH)
     self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, OUTPUT_PNG_FLAG, mock.ANY, mock.ANY])
 
 
+  def test_get_png_from_svg__given_width__calls_with_width_flag_and_value(self):
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH, EXPECTED_WIDTH)
+    self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, mock.ANY, WIDTH_FLAG, EXPECTED_WIDTH])
+
+
+  def test_get_png_from_svg__given_height__calls_with_height_flag_and_value(self):
+    get_png_from_svg(SVG_FILE_PATH, OUTPUT_FILE_PATH, None, EXPECTED_HEIGHT)
+    self.subprocess.call.assert_called_with([mock.ANY, mock.ANY, mock.ANY, mock.ANY, mock.ANY, HEIGHT_FLAG, EXPECTED_HEIGHT])
