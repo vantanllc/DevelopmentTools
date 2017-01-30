40f637a1b83d9b0a4de52bb2312ce7f8e28d2222
Thinh Luong <guitarslacker@gmail.com>
2017-01-29 21:26:22 -0600

    refactor variable

--- inkscape/svg_to_png/get_png_from_svg.py
+++ inkscape/svg_to_png/get_png_from_svg.py
@@ -3,9 +3,11 @@ import subprocess
 INKSCAPE_COMMAND = "inkscape"
 NO_GUI_FLAG = "-z"
 OUTPUT_PNG_FLAG = "-e"
+SVG_SUFFIX = "svg"
+PNG_SUFFIX = "png"
   
 def get_png_from_svg(svg_file_path, output_file_path):
-  if svg_file_path.endswith("svg") and output_file_path.endswith("png"):
+  if svg_file_path.endswith(SVG_SUFFIX) and output_file_path.endswith(PNG_SUFFIX):
     return subprocess.call([INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path])
   else:
     return -1
