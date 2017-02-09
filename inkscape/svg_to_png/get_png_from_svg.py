e2cb40096b59a88b15fcae20f7ef2a93a71588a0
Thinh Luong <guitarslacker@gmail.com>
2017-02-09 17:42:34 -0600

    Only export with page area

--- inkscape/svg_to_png/get_png_from_svg.py
+++ inkscape/svg_to_png/get_png_from_svg.py
@@ -7,7 +7,6 @@ WIDTH_FLAG = "-w"
 HEIGHT_FLAG = "-h"
 DPI_FLAG = "-d"
 DPI = "90"
-DRAWABLE_FLAG = "-D"
 SVG_SUFFIX = "svg"
 PNG_SUFFIX = "png"
   
@@ -15,7 +14,6 @@ def get_png_from_svg(svg_file_path, output_file_path, width=None, height=None):
   if svg_file_path.endswith(SVG_SUFFIX) and output_file_path.endswith(PNG_SUFFIX):
     inkscape_call = [INKSCAPE_COMMAND, NO_GUI_FLAG, OUTPUT_PNG_FLAG, output_file_path, svg_file_path]
     inkscape_call += [DPI_FLAG, DPI]
-    inkscape_call += [DRAWABLE_FLAG]
     if width:
       inkscape_call += [WIDTH_FLAG, width] 
     if height:
